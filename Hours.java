254025b5708f22c35292b8d7aefe6acd52b4b180
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-12 20:45:37 -0500

    Solved Hours.java

--- Hours.java
+++ Hours.java
@@ -1,13 +1,26 @@
 class Hours extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A Program Hours.java that converts hours to days and hours
+  * @author: C. Chen
   */
   
   public void run() {
     
-    // start coding here
+    // Declare variables
+    int intTotalHours;
+    int intDays;
+    int intHours;
+
+    // Get values for hours
+    intTotalHours = readInt("Enter the total number of hours: ");
+
+    // Calculations
+    intDays = intTotalHours / 24;
+    intHours = intTotalHours % 24;
+
+    // Output
+    System.out.println(intTotalHours + " Hours = " + intDays + " days and " + intHours + " hours");
     
   }
 }
