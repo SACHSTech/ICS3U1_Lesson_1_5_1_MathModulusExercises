b6de6c399b80ec484849ce24a8da68dbb26f803c
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-12 20:45:56 -0500

    Solves Rads.java

--- Rads.java
+++ Rads.java
@@ -1,13 +1,22 @@
 class Rads extends ConsoleProgram {
 
   /**
-  * Description
+  * A program Rads.java that converts degrees to radians
   * @author:
   */
   
   public void run() {
+    // Declare variables
+    double dblDegrees;
+    double dblRadians;
+
+    // Get degrees from user
+    dblDegrees = readDouble("Enter the angle in degrees: ");
+
+    // Calculate radians
+    dblRadians = dblDegrees * Math.PI / 180;
     
-    // start coding here
-    
+    // Output radians
+    System.out.println(dblDegrees + " degrees = " + dblRadians + " radians");
   }
 }
