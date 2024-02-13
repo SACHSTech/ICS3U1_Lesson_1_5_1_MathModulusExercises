24f410f8d929e3df255f31264dca7e13c9e108fe
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-13 17:18:10 -0500

    Solved AreaSin.java

--- AreaSin.java
+++ AreaSin.java
@@ -1,13 +1,28 @@
 class AreaSin extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program AreaSin.java that calculates the area of a triangle given
+  * two sides and the contained angle
+  * @author: C. Chen
   */
   
   public void run() {
     
-    // start coding here
-    
+    // Declare variables
+    double dblSideA;
+    double dblSideB;
+    double dblAngleC;
+    double dblArea;
+
+    // Get side A, side B, and angle C from user
+    dblSideA = readDouble("Enter the length of Side A: ");
+    dblSideB = readDouble("Enter the length of side B: ");
+    dblAngleC = readDouble("Enter the angle of the contained angle C: ");
+
+    // Calculate the area using A = (a*b*sinC)/2
+    dblArea = (dblSideA*dblSideB*Math.sin(dblAngleC)) / 2;
+
+    // Output results
+    System.out.println("The area of the traingle is " + dblArea);
   }
 }
