c765040a154d463a5f925e092992564e3105ebeb
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-12 20:45:52 -0500

    Solved Minutes.java

--- Minutes.java
+++ Minutes.java
@@ -1,13 +1,29 @@
 class Minutes extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program Minutes.java that convers mintues to days, hours, and minutes
+  * @author: C. Chen
   */
   
   public void run() {
-    
-    // start coding here
-    
+    // Declare variables
+    int intTotalMinutes;
+    int intDays;
+    int intHours;
+    int intMinutes;
+
+    // Get minutes from user
+    intTotalMinutes = readInt("Enter the number of minutes: ");
+
+    // Calculate number of days, hours, and minutes
+    intDays = intTotalMinutes / 1440;
+    intHours = (intTotalMinutes % 1440) / 60;
+    intMinutes = (intTotalMinutes % 1440) % 60;
+
+    // Output conversion
+    System.out.println(intTotalMinutes + " minutes = " + 
+                      intDays + " day(s), " + 
+                      intHours + " hour(s) and " + 
+                      intMinutes + " minute(s)");
   }
 }
