0aa1e8b7cb494bf2195d030a2cbe33eea510048f
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-12 20:45:46 -0500

    Solved Circumference.java

--- Circumference.java
+++ Circumference.java
@@ -1,13 +1,23 @@
 class Circumference extends ConsoleProgram {
 
   /**
-  * Description
+  * A program Circumference.java that calculates the circumference
+  * of a circle given a radius.
   * @author:
   */
   
   public void run() {
-    
-    // start coding here
-    
+    // Declare variables
+    double dblRadius;
+    double dblCircumference;
+
+    // Get radius from user
+    dblRadius = readDouble("Enter the radius of the circle: ");
+
+    // Calculation circumeference = 2*pi*r
+    dblCircumference = 2 * Math.PI * dblRadius;
+
+    // Output results
+    System.out.println("The circumference of the circle is: " + dblCircumference);
   }
 }
