b954988a601379e3794f7847d9030d6620efc93d
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-13 17:18:22 -0500

    Solved FtoC.java

--- FtoC.java
+++ FtoC.java
@@ -1,13 +1,23 @@
 class FtoC extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program FtoC.java that converts Fahrenheit to Celsius
+  * @author: C. Chen
   */
   
   public void run() {
     
-    // start coding here
-    
+    // Declare variables
+    double dblFahrenheit;
+    double dblCelsius;
+
+    // Get Fahrenheit from user
+    dblFahrenheit = readDouble("Enter the temperature in Fahrenheit: ");
+
+    // Calculate celsius: C = (5/9)(F - 32)
+    dblCelsius = (5.0/9) * (dblFahrenheit-32);
+
+    // Output results
+    System.out.println("The temperature in Celsius is " + dblCelsius + " degrees.");
   }
 }
