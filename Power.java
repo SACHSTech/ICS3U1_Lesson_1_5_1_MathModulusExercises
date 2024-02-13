7bd216f7d7e743e700b73232548a7bd0abcdc770
mschen-sta <cherrie.chen@ycdsb.ca>
2024-02-13 17:18:37 -0500

    Solved Power.java

--- Power.java
+++ Power.java
@@ -1,13 +1,25 @@
 class Power extends ConsoleProgram {
 
   /**
-  * Description
+  * A program Power.java that takes in two numbers a and b and calculates a^b
   * @author:
   */
   
   public void run() {
     
-    // start coding here
+    // Declare variables
+    int intBase;
+    int intExponent;
+    double dblPower;
+
+    // Get base and exponent from user
+    intBase = readInt("Enter the base: ");
+    intExponent = readInt("Enter the exponent: ");
     
+    // Calculate the base^exponent
+    dblPower = Math.pow(intBase, intExponent);
+
+    // Output results
+    System.out.println(intBase + " to the power of " + intExponent + " is " + dblPower);
   }
 }
