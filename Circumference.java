266e9a267f02ff954330a18e27b1602daeec18cd
NateJudd <nathaniel.judd25@ycdsbk12.ca>
2024-02-14 14:22:07 -0500

    Solved Circumference

--- Circumference.java
+++ Circumference.java
@@ -1,13 +1,23 @@
 class Circumference extends ConsoleProgram {
 
   /**
-  * Description
-  * @author:
+  * A program that computers the circumference of a circle given a radius from the user
+  * @author:N. Judd
   */
   
   public void run() {
     
-    // start coding here
-    
+    // declare variables
+    double dblRadius;
+    double dblCircumference;
+
+    // get radius
+    dblRadius = readDouble("Enter the radius: ");
+
+    // calculate circumference
+    dblCircumference = 2 * Math.PI + dblRadius;
+
+    // output results
+    System.out.println("The circumference of a circle with radius " + dblRadius + " is " + dblCircumference);
   }
 }
